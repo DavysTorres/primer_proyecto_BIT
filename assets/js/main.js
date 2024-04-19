13be64ad3e1db5dcb02164b09ba1a3528b362cd1
jcast08 <jhoancamilo0812@hotmail.com>
2024-04-19 18:28:26 -0500

    agregado planifica.html

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
 let bodyDOM = document.querySelector("#pepe")
 let nav = document.querySelector("#navbar").offsetHeight
-let alturaContenido = parseInt(bodyDOM.offsetHeight) + 1
+let alturaContenido = parseInt(bodyDOM.offsetHeight) + 100
 let cortinaBody = document.querySelector(".cortina")
 cortinaBody.style.height = alturaContenido + "px"
 cortinaBody.style.top = nav + "px"
